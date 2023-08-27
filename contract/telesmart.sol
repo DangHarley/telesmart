10f6864f98e89ee3923dd7b52e7c415917728d28
EastOrb <102253515+EastOrb@users.noreply.github.com>
2023-08-27 19:05:11 +0100

    Update telesmart.sol
    Used a onlyOwner modifier to restrict certain functions to the contract owner.
    Initialized the sold field to 0 within the struct.
    Improved error messages for clarity.
    Used a constant TelevisionInfo struct name to avoid conflicts.
    Made the televisions mapping private for encapsulation.
    Utilized a more efficient mapping-based approach in the getTelevision function.
    Improved naming consistency for functions.
    Removed unnecessary storage of televisionExists by checking price in placeOrder.

--- contract/telesmart.sol
+++ contract/telesmart.sol
@@ -1,80 +1,58 @@
 // SPDX-License-Identifier: MIT
 
-/** @title Television contract
- *  @notice Contract in charge of the Television marketplace
- */
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Television {
-    uint256 internal televisionsLength = 0;
-    address internal owner;
+    address private owner;
+    uint256 private televisionsLength = 0;
 
     struct Order {
         uint256 televisionId;
         uint256 count;
     }
 
-    struct Television {
+    struct TelevisionInfo {
         string name;
         string image;
         uint256 price;
         uint256 sold;
     }
 
-    // mapping of television ids to the television details
-    mapping(uint256 => Television) internal televisions;
+    mapping(uint256 => TelevisionInfo) private televisions;
 
-    // mapping to show if television exists
-    mapping(uint256 => bool) internal televisionExists;
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this");
+        _;
+    }
 
-    /**
-     * @notice Constructor that sets owner to who deploys the contract
-     */
     constructor() {
         owner = msg.sender;
     }
 
-    /**
-     * @notice adds new television to television contract
-     * @param _name: television name
-     * @param _image: television image
-     * @param _price: television price
-     */
     function addNewTelevision(
         string memory _name,
         string memory _image,
         uint256 _price
-    ) public {
+    ) public onlyOwner {
         require(_price > 0, "Price must be greater than 0");
-        uint256 _sold = 0;
-        televisions[televisionsLength] = Television(_name, _image, _price, _sold);
-        televisionExists[televisionsLength] = true;
+        televisions[televisionsLength] = TelevisionInfo(_name, _image, _price, 0);
         televisionsLength++;
     }
 
-    /**
-     * @notice places orders
-     * @param _orders: pizza order
-     */
     function placeOrder(Order[] memory _orders) public payable {
-        uint256 _totalAmount;
+        uint256 totalAmount;
         for (uint256 i = 0; i < _orders.length; i++) {
             Order memory _order = _orders[i];
-            Television storage _television = televisions[_order.televisionId];
-            require(televisionExists[_order.televisionId] == true, "Television does not exist");
-            _totalAmount += _television.price * _order.count;
+            TelevisionInfo storage _television = televisions[_order.televisionId];
+            require(_television.price > 0, "Television does not exist");
+            totalAmount += _television.price * _order.count;
             _television.sold += _order.count;
         }
-        require(_totalAmount == msg.value, "Invalid Amount Sent");
-        // transfer amount
-        (bool success, ) = payable(owner).call{value: msg.value}("");
+        require(totalAmount == msg.value, "Invalid amount sent");
+        (bool success, ) = owner.call{value: msg.value}("");
         require(success, "Transfer of order amount failed");
     }
 
-    /**
-     * @notice returns television information
-     * @param _index: television index
-     */
     function getTelevision(uint256 _index)
         public
         view
@@ -85,18 +63,11 @@ contract Television {
             uint256
         )
     {
-        return (
-            televisions[_index].name,
-            televisions[_index].image,
-            televisions[_index].price,
-            televisions[_index].sold
-        );
+        TelevisionInfo storage tv = televisions[_index];
+        return (tv.name, tv.image, tv.price, tv.sold);
     }
 
-    /**
-     * @notice returns the amount of televisions in the contract
-     */
-    function getTelevisionslength() public view returns (uint256) {
-        return (televisionsLength);
+    function getTelevisionsLength() public view returns (uint256) {
+        return televisionsLength;
     }
 }
